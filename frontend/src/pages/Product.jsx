b87653e0963e2f770d1b6c9291ca4decc9093922
Shaheen <shn.sdq29@gmail.com>
2026-01-16 21:31:49 +0530

    added searchBar feature

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
+
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData,setProductData] = useState(false);
+  
+
+  const fetchProductData = async () => {
+     products.map((item)=>{
+      if(item._id === productId){
+        setProductData(item)
+        return null;
+      }
+     })
+  }
+
+  useEffect(()=>{
+
+  },[productId,products])
+
   return (
     <div>Product</div>
   )
