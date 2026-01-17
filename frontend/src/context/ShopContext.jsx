85f0a1e06e4c72c077f9dacd039eed07f6304711
Shaheen <shn.sdq29@gmail.com>
2026-01-17 23:22:47 +0530

    commit

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,11 +8,32 @@ const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
-    const [showSearch, setShowSearch] = useState(true)
+    const [showSearch, setShowSearch] = useState(true);
+    const [cartItems,setCartItems] = useState({});
+
+    const addToCart = async (itemId,size) =>{
+        
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] += 1;
+            }
+            else{
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems,addToCart
     }
 
     return (
