1d4903015634067ec3f34a6fcf6c0980971f412c
Shaheen <shn.sdq29@gmail.com>
2026-01-19 19:41:17 +0530

    commit

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -53,11 +53,33 @@ const ShopContextProvider = (props) => {
         return totalCount;
      }
 
+     const updateQuantity = async (itemId,size,quantity)=>{
+       let cartData= structuredClone(cartItems);
+
+       cartData[itemId][size] = quantity;
+
+       setCartItems(cartData);
+     }
+
+     const getCartAmount = async()=>{
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo = products.find((product)=>product._id === items);
+            for(const item in cartItems[items]){
+                try {
+                    if (cartItems)
+                } catch (error) {
+                    
+                }
+            }
+        }
+     }
+
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems,addToCart,
-        getCartCount
+        getCartCount,updateQuantity
     }
 
     return (
