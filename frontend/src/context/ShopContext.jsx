d00d8b48c921337954e8a831f05507f89f86b7ee
Shaheen <shn.sdq29@gmail.com>
2026-01-20 21:29:52 +0530

    cart funtionality completed

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -61,25 +61,29 @@ const ShopContextProvider = (props) => {
        setCartItems(cartData);
      }
 
-     const getCartAmount = async()=>{
+     const getCartAmount = ()=>{
         let totalAmount = 0;
         for(const items in cartItems){
             let itemInfo = products.find((product)=>product._id === items);
             for(const item in cartItems[items]){
                 try {
-                    if (cartItems)
+                    if (cartItems[items][item] > 0){
+                        totalAmount += itemInfo.price *  cartItems[items][item]
+                    }
                 } catch (error) {
                     
                 }
             }
         }
+        return totalAmount;
      }
 
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems,addToCart,
-        getCartCount,updateQuantity
+        getCartCount,updateQuantity,
+        getCartAmount
     }
 
     return (
