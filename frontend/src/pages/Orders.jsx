321233f03b8caa48c8242ee7695c751dc0e7eda9
Shaheen <shn.sdq29@gmail.com>
2026-01-21 23:16:47 +0530

    commit

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
 
 const Orders = () => {
+
+  const {currency,products} = useContext(ShopContext);
+
   return (
-    <div>Orders</div>
+    <div className='border-t pt-16'>
+      <div className='text-2xl '>
+        <Title text1={'MY'} text2={'ORDERS'} />
+      </div>
+
+      <div>
+        {
+          products.slice(1,4).map((item,index)=>(
+            <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4' >
+             <div className='flex items-start gap-6 text-sm'>
+              <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
+              <div>
+                <p className='sm:text-base font-medium'>{item.name} </p>
+                <div className='flex item-center gp-3 mt-2'>
+
+                </div>
+              </div>
+             </div>
+            </div>
+          ))
+        }
+      </div>
+
+    </div>
   )
 }
 
