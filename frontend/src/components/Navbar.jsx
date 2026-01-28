b8b73f43b07b9fad173c27176c08e35c5325fddf
Shaheen <shn.sdq29@gmail.com>
2026-01-28 22:29:45 +0530

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
 
-    const [visible,setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
 
-    const {setShowSearch,getCartCount} = useContext(ShopContext);
+    const { setShowSearch, getCartCount } = useContext(ShopContext);
 
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
@@ -36,7 +36,7 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-6'>
-                <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
+                <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
                 <div className='group relative' >
                     <Link to='/login'> <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
@@ -49,23 +49,23 @@ const Navbar = () => {
                     </div>
                 </div>
                 <Link to='/cart' className='relative'>
-                <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
-                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px] '>{getCartCount()}</p>
+                    <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px] '>{getCartCount()}</p>
                 </Link>
-                <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+                <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
             </div>
             {/* Sidebar menu for small screens */}
-            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible?'w-full' : 'w-0'}`} >
-<div className='flex flex-col text-gray-600'>
-<div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
-<img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
-<p>Back</p>
-</div>
-<NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
-<NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
-<NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
-<NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
-</div>
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`} >
+                <div className='flex flex-col text-gray-600'>
+                    <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                        <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+                        <p>Back</p>
+                    </div>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+                </div>
             </div>
         </div>
     )
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center py-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+      <img className='w-full md:max-w-[450px] ' src={assets.about_img} alt="" />
+      <div className='flex flex-col justify-center '>
+
+      </div>
+      </div>
+    </div>
   )
 }
 
