2a9bb3480354b0e924322b7ce1713d0a27466a58
Shaheen <shn.sdq29@gmail.com>
2026-01-29 22:25:18 +0530

    commit

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from '../components/Title'
+import NewsletterBox from '../components/NewsletterBox'
 import { assets } from '../assets/assets'
 
 const About = () => {
@@ -11,10 +12,33 @@ const About = () => {
       </div>
       <div className='my-10 flex flex-col md:flex-row gap-16'>
       <img className='w-full md:max-w-[450px] ' src={assets.about_img} alt="" />
-      <div className='flex flex-col justify-center '>
+      <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, facilis?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est porro sunt harum tempora officia rerum nemo maiores autem et id!</p>
+      <b className='text-gray-800'>Our Mission</b>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ducimus repellendus, magni reprehenderit sunt pariatur labore nesciunt fugiat non cumque!</p>
+      </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
 
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+           <b>Quality Assurance</b>
+           <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sint quis molestias possimus amet aliquid debitis aperiam fuga ipsam quasi?</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+           <b>Convenience:</b>
+           <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sint quis molestias possimus amet aliquid debitis aperiam fuga ipsam quasi?</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+           <b>Exceptional Customer Service</b>
+           <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sint quis molestias possimus amet aliquid debitis aperiam fuga ipsam quasi?</p>
+        </div>
       </div>
-      </div>
+
+      <NewsletterBox/>
     </div>
   )
 }
