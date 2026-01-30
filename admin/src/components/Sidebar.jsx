ef8eaf39653d92f47f62b7d76cca2bdf451a23cf
Shaheen <shn.sdq29@gmail.com>
2026-01-30 22:17:39 +0530

    added admin panel

---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div>
+        <div>
+            <NavLink to='/add'>
+
+            </NavLink>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
