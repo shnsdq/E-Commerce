4812c87dd0ede38b1dcb7de9ca3361bf7f612ae0
Shaheen <shn.sdq29@gmail.com>
2026-01-31 22:38:26 +0530

    commit

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <form>
+       <div>
+        <p>Upload Image</p>
+
+        <div>
+          <label htmlFor="image1">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image1" hidden/>
+          </label>
+          <label htmlFor="image1">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image1" hidden/>
+          </label>
+        </div>
+       </div>
+    </form>
   )
 }
 
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary} from "cloudinary"
-import productModel from '../models.productModels.js'
+import productModel from '../models/productModel.js'
 
 //function for add product
 const addProduct = async (req,res) => {
