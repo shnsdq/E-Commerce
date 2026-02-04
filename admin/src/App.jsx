c5595824d96cb27f3186be0591c1727362b6077d
Shaheen <shn.sdq29@gmail.com>
2026-02-04 15:37:13 +0530

    commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './components/Login'
   
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const currency = '$'
 
 const App = () => {
 
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import {useNavigate} from "react-router-dom"
+import axios from 'axios';
 
 export const ShopContext = createContext();
 
@@ -9,9 +9,12 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(true);
     const [cartItems,setCartItems] = useState({});
+    const [products,setProducts] = useState([]);
+    const [token,setToken] = useState('');
     const navigate = useNavigate();
 
     const addToCart = async (itemId,size) =>{
@@ -80,12 +83,33 @@ const ShopContextProvider = (props) => {
         return totalAmount;
      }
 
+     const getProductsData = async () =>{
+        try {
+            
+       const response = await axios.get(backendUrl + '/api/product/list')
+       if (response.data.success) {
+        setProducts(response.data.products);
+      } else {
+        toast.error(response.data.message)
+      }
+
+        } catch (error) {
+        console.log(error)
+        toast.error(error.message)
+        }
+     }
+
+     useEffect(()=>{
+     getProductsData()
+     },[])
+
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems,addToCart,
         getCartCount,updateQuantity,
-        getCartAmount,navigate
+        getCartAmount,navigate,backendUrl,
+        token,setToken
     }
 
     return (
