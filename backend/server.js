c65b65ae126c2cf32ad7550d907e6026d7720861
Shaheen <shn.sdq29@gmail.com>
2026-07-12 22:41:58 +0530

    fix: update CORS configuration to allow requests from the frontend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:"https://e-commerce-forever-frontend-byxutuwvk.vercel.app/",
+    credentials: true
+}));
 
 // api endpoints
 app.use('/api/user',userRouter)
