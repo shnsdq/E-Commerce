e3b810f098509406bee1d5350d476bebb23703f3
Shaheen <shn.sdq29@gmail.com>
2026-07-13 12:25:41 +0530

    CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,7 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors({
-    origin:"https://e-commerce-forever-frontend-seven.vercel.app",
-    credentials: true
-}));
+app.use(cors());
 
 // api endpoints
 app.use('/api/user',userRouter)
