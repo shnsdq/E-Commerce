4c41724e3e09ea0e704970d71eea217c25ed49d1
Shaheen <shn.sdq29@gmail.com>
2026-09-04 19:27:51 +0530

    improved code formatting

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -28,12 +28,12 @@ const Orders = ({ token }) => {
     }
   }
 
-  const statusHandler = async(event, orderId) =>{
+  const statusHandler = async (event, orderId) => {
     try {
-       const response = await axios.post(backendUrl + '/api/order/status', {orderId, status:event.target.value}, { headers: { token } })
-         if (response.data.success) {
-          await fetchAllOrders()
-         }
+      const response = await axios.post(backendUrl + '/api/order/status', { orderId, status: event.target.value }, { headers: { token } })
+      if (response.data.success) {
+        await fetchAllOrders()
+      }
     } catch (error) {
       console.log(error)
       toast.error(error.message)
@@ -72,11 +72,11 @@ const Orders = ({ token }) => {
               <div>
                 <p className='text-sm sm:text-[15px] '>Items : {order.items.length} </p>
                 <p className='mt-3'>Method : {order.paymentMethod}</p>
-                <p>Payment : {order.payment ? 'Done': 'Pending'}</p>
+                <p>Payment : {order.payment ? 'Done' : 'Pending'}</p>
                 <p>Date : {new Date(order.date).toLocaleDateString()}</p>
               </div>
               <p className='text-sm sm:text-[15px] '>{currency}{order.amount} </p>
-              <select onChange={(event)=>statusHandler(event,order._id)} value={order.status} className='p-2 font-semibold'>
+              <select onChange={(event) => statusHandler(event, order._id)} value={order.status} className='p-2 font-semibold'>
                 <option value="Order Placed">Order Placed</option>
                 <option value="Packing">Packing</option>
                 <option value="Shipped">Shipped</option>
